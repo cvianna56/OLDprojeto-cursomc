2ee1242991377b38436e3799a6c14993aa97d311
Carlos Eugenio <cvianna56@gmail.com>
2022-08-03 15:38:18 -0300

    Projeto criado

---
+++ src/main/java/com/carlos/cursomc/ProjetoCursomcApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCursomcApplication.class, args);
+	}
+
+}
